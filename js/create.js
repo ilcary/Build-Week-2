7611feb50f3f6284d26c3d22afa79c475ab59fca
Sergio19702 <98604139+Sergio19702@users.noreply.github.com>
2022-08-09 15:45:06 +0200

    aggiornamento

--- js/create.js
+++ js/create.js
@@ -12,52 +12,117 @@ class Utente {
         this.gender = __gender;
         this.email = __email;
         this.profileURL = __profileURL;
-        this.fatture={};
+        this.fatture = {};
         this.takeForm()
 
     }
-     takeForm(){
-    this.username = document.querySelector('#username').value
-    this.btd = document.querySelector('#btd').value
-    this.firstname = document.querySelector('#firstname').value
-    this.lastname = document.querySelector('#lastname').value
-    this.gender = document.querySelector('#gender').value
-    this.email = document.querySelector('#email').value
-    this.checkgender()
-}
-checkgender(){
-    if (document.querySelector('#gender').value == 'Male') {
-        this.profileURL= "img/male.png"
-    }else if(document.querySelector('#gender').value == 'Female'){
-        this.profileURL= "img/female.png" 
-    }else{
-        this.profileURL= "img/helicopter.png"
+    takeForm() {
+        this.username = document.querySelector('#username').value
+        this.btd = document.querySelector('#btd').value
+        this.firstname = document.querySelector('#firstname').value
+        this.lastname = document.querySelector('#lastname').value
+        this.gender = document.querySelector('#gender').value
+        this.email = document.querySelector('#email').value
+        this.checkgender()
+    }
+    checkgender() {
+        if (document.querySelector('#gender').value == 'Male') {
+            this.profileURL = "img/male.png"
+        } else if (document.querySelector('#gender').value == 'Female') {
+            this.profileURL = "img/female.png"
+        } else {
+            this.profileURL = "img/helicopter.png"
+        }
     }
 }
-}
 
-button.addEventListener('click',function(e){
+function validate(form) {
+    console.log(form)
+
+    if (form.username.value == "") {
+        swal({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Please provide your Nickname!",
+          })
+        form.username.focus();
+        return false;
+    }
+    if (form.dateofbirth.value == "") {
+        swal({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Please provide your date of birth!",
+          })
+        form.dateofbirth.focus();
+        return false;
+    }
+    if (form.firstname.value == "") {
+        swal({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Please provide your first name!",
+          })
+        form.firstname.focus();
+        return false;
+    }
+    if (form.lastname.value == "") {
+        swal({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Please provide your last name!",
+          })
+        form.lastname.focus();
+        return false;
+    }
+    var emailID = form.email.value;
+         atpos = emailID.indexOf("@");
+         dotpos = emailID.lastIndexOf(".");
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+        swal({
+            icon: 'error',
+            title: 'Oops...',
+            text: "Please enter correct email",
+          })
+        form.email.focus() ;
+        return false;
+     }
+     return( true );
+    }
+
+         
+
+
+
+
+button.addEventListener('click', function (e) {
     e.preventDefault();
+
+    if (!(validate(creazioneUtente)))
+        return
 
     let user = new Utente(username, btd, firstname, lastname, gender, email);
 
     let options = {
         method: 'POST',
         body: JSON.stringify(user),
-        headers:{
-            "content-type":"application/json"
+        headers: {
+            "content-type": "application/json"
         }
     }
-    
-    fetch(apiUtenti,options)//
-    .then(res => res.json())
-    .then(res => {
-        swal({
-            icon: 'success',
-            title: 'Good job bro a new user has been created!',
-            text:`The user ${res.firstname} ${res.lastname} with id: ${res.id} has been created!`,
-            showConfirmButton: false,
-            timer: 5000
-        }).then(() => location.href = 'index.html')
-    })
+
+    fetch(apiUtenti, options)//
+        .then(res => res.json())
+        .then(res => {
+            swal({
+                icon: 'success',
+                title: 'Good job bro a new user has been created!',
+                text: `The user ${res.firstname} ${res.lastname} with id: ${res.id} has been created!`,
+                showConfirmButton: false,
+                timer: 5000
+            }).then(() => location.href = 'index.html')
+        })
+
 })
+
+
