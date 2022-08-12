a655f229df565a04195270d52a1b19db0e10a128
ilCary <carisiodigiampietro54@gmail.com>
2022-08-12 15:43:36 +0200

    patch css ready night toggle

--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,19 @@
-var checkbox = document.querySelector('input[name=mode]');
-
-checkbox.addEventListener('change', function () {
-    if (this.checked) {
-        trans()
+let checkbox = document.querySelector('input[name=mode]');
+let checked = localStorage.getItem('nightmode') ? JSON.parse(localStorage.getItem('nightmode')) : [];
+checkbox.checked = checked
+function checknighton(){
+    if (checkbox.checked) {
+        /* trans() */
         document.body.setAttribute('data-theme', 'darkTheme')
     } else {
-        trans()
+        /* trans() */
         document.body.setAttribute('data-theme', 'lightTheme')
     }
-})
+    localStorage.setItem('nightmode', checkbox.checked)
+}
+window.onload= checknighton();
+checkbox.addEventListener('change',checknighton)
+
 
 let trans = () => {
     document.body.classList.add('transition');
